fda2131f1b1e1c23157c32dda3f0889afccedd8e
Oscar Lindqvist <olindqv@gmail.com>
2021-01-19 23:47:28 +0100

    add sqlite database

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const sampleData = require('./sample-data/sample-poems.json');
+const express = require("express");
+const sampleData = require("./sample-data/sample-poems.json");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize({ dialect: "sqlite", storage: "poems.db" });
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Failed database connection", error);
+  }
+})();
 
 const app = express();
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index', { poems: sampleData } );
+app.get("/", (req, res) => {
+  res.render("index", { poems: sampleData });
 });
 
-app.listen(3000, () => {console.log("The server is running")});
+app.listen(3000, () => {
+  console.log("The server is running");
+});
