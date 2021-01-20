6e7ab60db47b7a7323ddf4031367d12bd60e7a5b
Oscar Lindqvist <olindqv@gmail.com>
2021-01-20 07:53:41 +0100

    add poem table definition

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const sampleData = require("./sample-data/sample-poems.json");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize({ dialect: "sqlite", storage: "poems.db" });
 
+// Poem model
+class Poem extends Sequelize.Model {}
+Poem.init(
+  {
+    content: Sequelize.STRING,
+    date: Sequelize.DATE,
+  },
+  { sequelize }
+);
+
 (async () => {
   try {
-    await sequelize.authenticate();
-    console.log("Connected to database");
+    await sequelize.sync({ force: true });
+    console.log("Successfully synced");
   } catch (error) {
-    console.error("Failed database connection", error);
+    console.error("Error connecting to database", error);
   }
 })();
 
