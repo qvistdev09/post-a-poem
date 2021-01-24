693c77e210c200fadc49e926f9549910500775be
Oscar Lindqvist <olindqv@gmail.com>
2021-01-24 22:10:28 +0100

    add poem word buttons

--- public/app.js
+++ public/app.js
@@ -1,1 +1,41 @@
-console.log('File is loaded');
+console.log("File is loaded");
+const wordPalette = document.getElementById("word-palette");
+const composedPoem = document.getElementById("composed-poem");
+
+const addWord = button => {
+  button.setAttribute("disabled", "true");
+  const poemWordBtn = document.createElement("button");
+  poemWordBtn.innerText = button.innerText;
+  poemWordBtn.setAttribute("onclick", "removeWord(this)");
+  poemWordBtn.setAttribute("id", button.innerText + "-rm");
+  composedPoem.appendChild(poemWordBtn);
+};
+
+const removeWord = button => {
+  const correspondingAddButton = document.getElementById(
+    button.innerText + "-add"
+  );
+  correspondingAddButton.removeAttribute("disabled");
+  composedPoem.removeChild(button);
+};
+
+const paletteWords = [
+  "silence",
+  "potato",
+  "is",
+  "justice",
+  "would",
+  "alarmingly",
+  "tremor",
+  "warned",
+  "in",
+  "places",
+];
+
+for (let word of paletteWords) {
+  const wordButton = document.createElement("button");
+  wordButton.innerText = word;
+  wordButton.setAttribute("id", word + "-add");
+  wordButton.setAttribute("onclick", "addWord(this)");
+  wordPalette.appendChild(wordButton);
+}
