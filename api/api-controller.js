055259afbcf7f74b031440ff97a60da59134477a
Oscar Lindqvist <olindqv@gmail.com>
2021-02-07 11:25:05 +0100

    setup correct api status codes

--- api/api-controller.js
+++ api/api-controller.js
@@ -10,14 +10,14 @@ api.get('/poems', (req, res) => {
       res.json(poems);
     })
     .catch(error => {
-      errorResponse(res, error);
+      errorResponse(res, error, 500);
     });
 });
 
 api.post('/poems', (req, res) => {
   const clientInput = req.body.input;
   if (!clientInput) {
-    errorResponse(res, { message: 'Empty input' });
+    errorResponse(res, { message: 'Empty input' }, 400);
   } else {
     poemsService
       .postPoem(clientInput)
@@ -25,7 +25,7 @@ api.post('/poems', (req, res) => {
         res.json(response);
       })
       .catch(error => {
-        errorResponse(res, error);
+        errorResponse(res, error, 400);
       });
   }
 });
@@ -37,11 +37,12 @@ api.get('/words', (req, res) => {
       res.json(data);
     })
     .catch(error => {
-      errorResponse(res, error);
+      errorResponse(res, error, 500);
     });
 });
 
-const errorResponse = (res, error) => {
+const errorResponse = (res, error, statusCode) => {
+  res.statusCode = statusCode;
   res.json({ error: error.message });
 };
 
